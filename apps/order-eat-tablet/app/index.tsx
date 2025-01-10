28ee41d2ea77be9f883dbf0985a6c4d6c79a6d72
Karim CHARLEUX <karim.charleux@hotmail.fr>
2025-01-10 23:57:01 +0100

    #45 lock screen to landscape on tablet command

--- apps/order-eat-tablet/app/index.tsx
+++ apps/order-eat-tablet/app/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import TabletScannerPage from '@/components/TabletScannerPage';
+import * as ScreenOrientation from "expo-screen-orientation";
+import { Platform } from 'react-native';
+if (Platform.OS !== 'web'){
+  ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.DEFAULT);
+}
 
 export default function Index() {
   return (
--- apps/order-eat-tablet/app/restaurant/index.tsx
+++ apps/order-eat-tablet/app/restaurant/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useLocalSearchParams } from 'expo-router';
 import TabletMenu from '@/components/TabletMenu';
+import * as ScreenOrientation from "expo-screen-orientation";
+import { Platform } from 'react-native';
+
 
 export default function RestaurantScreen() {
   const { id } = useLocalSearchParams();
-
+  if (Platform.OS !== 'web'){
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+  }
   return <TabletMenu restaurantId={id as string} />;
 }
 
